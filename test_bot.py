68c366ec46a14a11895f9f6b8b8b2117c83c8574
24ndres <andres.marin.abad+github@gmail.com>
2017-10-18 13:26:25 +0200

    Debug...

--- test_bot.py
+++ test_bot.py
@@ -26,41 +26,34 @@ class BotHandler:
         if len(get_result) > 0:
             last_update = get_result[-1]
         else:
-            last_update = get_result[len(get_result)]
+            #last_update = get_result[len(get_result)]
+            last_update = None
 
         return last_update
 
-greet_bot = BotHandler('token')
-greetings = ('hello', 'hi', 'greetings', 'sup')
-now = datetime.datetime.now()
+greet_bot = BotHandler('466425736:AAHFy55FZHxJ4pbPYEpOEF9gc5DdaLCqmCU')
+commands = ('hello')
 
 
 def main():
     new_offset = None
-    today = now.day
-    hour = now.hour
 
     while True:
         greet_bot.get_updates(new_offset)
 
         last_update = greet_bot.get_last_update()
+        last_chat_text = ''
+        last_update_id = -1
+
+        if last_update:
                 
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
+            last_update_id = last_update['update_id']
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['chat']['first_name']
 
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
-            today += 1
+        if last_chat_text.lower() in commands:
+            greet_bot.send_message(last_chat_id, 'Hello  {}'.format(last_chat_name))
 
         new_offset = last_update_id + 1
 
