932c808a0d2821cf241bfd90ff8c2a0cb6eea315
24ndres <andres.marin.abad+github@gmail.com>
2017-10-18 16:19:21 +0200

    Debug

--- test_bot.py
+++ test_bot.py
@@ -31,8 +31,6 @@ class BotHandler:
         return last_update
 
 greet_bot = BotHandler('466425736:AAHFy55FZHxJ4pbPYEpOEF9gc5DdaLCqmCU')
-#commands = ['euca','eucaristia','eucaristía','palabra', 'convi', 'convivencia', 'avisos', 'grupos', 'temas', 'tema', 'lista', 'admin']
-euca = ['euca','eucaristia','eucaristía', '/euca']
 
 def main():
     new_offset = None
@@ -49,8 +47,11 @@ def main():
             last_chat_id = last_update['message']['chat']['id']
             last_chat_name = last_update['message']['chat']['first_name']
 
-            #if any(words in euca for words in last_chat_text.lower()):
-            msg = 'Hola '+last_chat_name+', la siguiente eucaristía es el sábado a las 21:00 en la Iglesia.'
+            if last_chat_text.lower() == '/euca':
+                msg = 'Hola '+last_chat_name+', la siguiente eucaristía es el sábado a las 21:00 en la Iglesia.'
+            else:
+                msg = 'Hola '+last_chat_name+', no me has dado la instrucción correcta.'
+
             greet_bot.send_message(last_chat_id, msg)
 
             new_offset = last_update_id + 1
